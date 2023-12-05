8a7d3c39309044136ed0d30570e1d55a6ae5eaf2
Antonio Gagliardo <ajgagliardo92@gmail.com>
2023-12-05 15:10:15 -0800

    first commit, just created 1 tile and playing with it

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log('hello world');
+
+
+const tile = document.createElement('div');
+tile.classList.add('tile');
+
+
+
+const grid = document.querySelector('.grid');
+grid.appendChild(tile);
+grid.appendChild(tile);
+
+
+
+
+//TO do
+
+//First the click and change color , i ahve to work on that
+// Then check how to put various tiles in the same row, i guess adding with flex, maybe having to add with  different names (for i range  and tile1,2,3 = tile (but blank))
+
+// I am htinking on trying something like for loop where i put tile 1,2,3,4,5 etc using (i) ... i would be i++, ma range will be range.lenght 
+// range would be what the user asks the amount of tiles to be
+// then I would also need to put at the begining of the function to reset all colors and actions with the div
